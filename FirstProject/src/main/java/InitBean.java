da334f5a7aa280087f2c6936b56f88d570f18d6a
csteyrer <christian.steyrer@hotmail.com>
2018-09-24 09:36:16 +0200

    nvs-assignment01
    Erstes Projekt mit WildFly

---
+++ FirstProject/src/main/java/InitBean.java
@@ -0,0 +1,13 @@
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@Startup
+
+public class InitBean {
+    @PostConstruct
+    private void init(){
+        System.out.println("*** it works ***");
+    }
+}
